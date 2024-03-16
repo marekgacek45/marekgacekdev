2616492beb1b7b881dd89e9314a83564efffb78f
gacmen45 <marekgacek45@gmail.com>
2024-03-16 16:48:59 +0100

    fix bug with duplicate tittle

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -23,7 +23,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title inertia>Marek Gacek - Strony Internetowe & Marketing | Odkryj wszechświat możliwości online</title>
+  
    
     
     <!--FAVICONS-->
