92ceb3271cc8d33628600a5642a768ad7a334a8d
gacmen45 <marekgacek45@gmail.com>
2024-03-30 12:53:03 +0100

    update icon in admin sidebar

--- resources/js/Shared/Admin/SideBar.vue
+++ resources/js/Shared/Admin/SideBar.vue
@@ -1,6 +1,6 @@
 <template>
-    <aside class="fixed bottom-0 left-0 top-0  bg-ownPurple-400 z-50" >
-        <div class="flex flex-col justify-center h-full px-4 py-4 ">
+    <aside class="fixed bottom-0 left-0 top-0 bg-ownPurple-400 z-50">
+        <div class="flex flex-col justify-center h-full px-4 py-4">
             <!-- LOGO-->
             <!-- <div>
 
@@ -10,32 +10,97 @@
                     <span class="font-heading text-4xl group-hover:text-ownPurple-400 duration-300 hidden xl:block">Gacek</span>
                 </Link>
             </div> -->
-            
+
             <!--ITEMS-->
             <div>
                 <ul class="space-y-4">
-           <SidebarItem ><img src="/assets/icons/pixel/kings_hat.svg" alt="" width="32"> <span class="hidden xl:inline-block">Dashboard</span></SidebarItem>
-           <SidebarItem :href="route('admin.post.index')"><img src="/assets/icons/pixel/open_chest.svg" alt="" width="32"> <span class="hidden xl:inline-block">Posty</span></SidebarItem>
-           <SidebarItem :href="route('admin.project.index')"><img src="/assets/icons/admin/project.svg" alt="" width="32"> <span class="hidden xl:inline-block">Projekty</span></SidebarItem>
-           <SidebarItem :href="route('admin.category.index')"><img src="/assets/icons/admin/category.svg" alt="" width="32"> <span class="hidden xl:inline-block">Kategorie</span></SidebarItem>
-           <SidebarItem :href="route('admin.testimonial.index')"><img src="/assets/icons/admin/comment.svg" alt="" width="32"> <span class="hidden xl:inline-block">Opinie</span></SidebarItem>
-           <SidebarItem :href="route('admin.tool.index')"><img src="/assets/icons/admin/technology.svg" alt="" width="32"> <span class="hidden xl:inline-block">Narzędzia</span></SidebarItem>
-           <SidebarItem ><img src="/assets/icons/admin/logout.svg" alt="" width="32" ><span class="hidden xl:inline-block">Wyloguj</span></SidebarItem>
-               <Link :href="route('logout')" method="post" as="button">Wyloguj</Link>
-               <Link :href="route('profile.edit')">edytuj profil</Link>
-              
-        </ul>
-    </div>
-    <ul>
-
-       
-    </ul>
+                    <SidebarItem
+                        ><img
+                            src="/assets/icons/admin/dashboard.png"
+                            alt=""
+                            width="32"
+                        />
+                        <span class="hidden xl:inline-block"
+                            >Dashboard</span
+                        ></SidebarItem
+                    >
+                    <SidebarItem :href="route('admin.post.index')"
+                        ><img
+                            src="/assets/icons/admin/post.png"
+                            alt=""
+                            width="32"
+                        />
+                        <span class="hidden xl:inline-block"
+                            >Posty</span
+                        ></SidebarItem
+                    >
+                    <SidebarItem :href="route('admin.project.index')"
+                        ><img
+                            src="/assets/icons/admin/project.png"
+                            alt=""
+                            width="32"
+                        />
+                        <span class="hidden xl:inline-block"
+                            >Projekty</span
+                        ></SidebarItem
+                    >
+                    <SidebarItem :href="route('admin.category.index')"
+                        ><img
+                            src="/assets/icons/admin/category.png"
+                            alt=""
+                            width="32"
+                        />
+                        <span class="hidden xl:inline-block"
+                            >Kategorie</span
+                        ></SidebarItem
+                    >
+                    <SidebarItem :href="route('admin.testimonial.index')"
+                        ><img
+                            src="/assets/icons/admin/opinion.png"
+                            alt=""
+                            width="32"
+                        />
+                        <span class="hidden xl:inline-block"
+                            >Opinie</span
+                        ></SidebarItem
+                    >
+                    <SidebarItem :href="route('admin.tool.index')"
+                        ><img
+                            src="/assets/icons/admin/tools.png"
+                            alt=""
+                            width="32"
+                        />
+                        <span class="hidden xl:inline-block"
+                            >Narzędzia</span
+                        ></SidebarItem
+                    >
+                    
+                    <SidebarItem>
+                        <img
+                            src="/assets/icons/admin/edit.png"
+                            alt=""
+                            width="32"
+                        /> <Link :href="route('profile.edit')">Edytuj Profil</Link>
+                    </SidebarItem>
+                    <SidebarItem>
+                        <img
+                            src="/assets/icons/admin/logout.png"
+                            alt=""
+                            width="32"
+                        /><Link :href="route('logout')" method="post" as="button"
+                        >Wyloguj</Link
+                    >
+                    </SidebarItem>
+                    
+                   
+                </ul>
+            </div>
+            <ul></ul>
             <!--ITEMS-->
         </div>
     </aside>
 </template>
 
-
 <script setup>
-import SidebarItem from '@/Components/Admin/SidebarItem.vue'
+import SidebarItem from "@/Components/Admin/SidebarItem.vue";
 </script>
